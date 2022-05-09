129fff5e4440924486c2e4148900b9a5556987eb
Gökay Soysal <agokaysoysal@gmail.com>
2022-05-09 22:24:31 +0300

    java class

---
+++ src/PerfectNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int toplam = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int k = input.nextInt();
+
+        for (int i = 1; i < k; i++) {
+            if (k % i == 0) {
+                toplam += i;
+            }
+        }
+        if (toplam == k) {
+            System.out.println("Number is perfect.");
+        } else
+            System.out.println("Number is not perfect.");
+    }
+}
